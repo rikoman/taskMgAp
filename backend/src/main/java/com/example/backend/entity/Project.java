c49f413bafe1f20681977dd5410aa0ebdfce90b6
roman <rikoman702@gmail.com>
2023-09-13 19:10:40 +0700

    Добавлеана связь с сущностью user

--- backend/src/main/java/com/example/backend/entity/Project.java
+++ backend/src/main/java/com/example/backend/entity/Project.java
@@ -1,9 +1,6 @@
 package com.example.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,4 +18,7 @@ public class Project {
     private String title;
     private boolean status;
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
