2fcd83f1a2467985e2873fd41f64385e990804b4
roman <rikoman702@gmail.com>
2023-09-13 18:48:41 +0700

    Создание сущности User,сервис CRUD User,DTO User,репозиторий User и контроллер User

---
+++ backend/src/main/java/com/example/backend/entity/User.java
@@ -0,0 +1,20 @@
+package com.example.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Usert")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+}
---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,35 @@
+package com.example.backend.service;
+
+import com.example.backend.DTO.UserDTO;
+import com.example.backend.entity.User;
+import com.example.backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User createUser(@RequestBody UserDTO dto){
+        User user = User.builder()
+                .name(dto.getName())
+                .build();
+        return userRepository.save(user);
+    }
+
+    public List<User> readAllUser(){
+        return userRepository.findAll();
+    }
+
+    public User updateUser (@RequestBody User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+}
