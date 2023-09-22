ff456e9031d224a66c4040dc3ca0f085fd7aac07
roman <rikoman702@gmail.com>
2023-09-22 12:32:25 +0700

    pojo для реализации JWT

---
+++ backend/src/main/java/com/example/backend/security/pojo/SignupRequest.java
@@ -0,0 +1,13 @@
+package com.example.backend.security.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+@Getter
+@Setter
+public class SignupRequest {
+    private String username;
+    private Set<String> roles;
+    private String password;
+}
