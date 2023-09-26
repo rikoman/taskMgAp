6e47652210c121dca5fbbea0f719f57258666794
roman <rikoman702@gmail.com>
2023-09-26 10:29:08 +0700

    Добавление новых методов в сервисе и контроллере, обрабатывающие новые запросы к базе

--- backend/src/main/java/com/example/backend/controller/TaskController.java
+++ backend/src/main/java/com/example/backend/controller/TaskController.java
@@ -8,26 +8,60 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/task")
 public class TaskController {
+
     private final TaskService taskService;
+
     @PostMapping
-    public ResponseEntity<Task> createTask(@RequestBody TaskDTO dto){
+    public ResponseEntity<Task> createTask(@RequestBody TaskDTO dto) throws ParseException {
+        if(dto.getDate()== null){
+            dto.setDate(String.valueOf(LocalDate.now()));
+        }
         return new ResponseEntity<>(taskService.createTask(dto), HttpStatus.OK);
     }
+
     @GetMapping
     public ResponseEntity<List<Task>> readAllTask(){
         return new ResponseEntity<>(taskService.readAllTask(),HttpStatus.OK);
+    }
+
+    @GetMapping("/true")
+    public ResponseEntity<List<Task>> readAllTaskByStatusTrue(){
+        return new ResponseEntity<>(taskService.readAllTaskByStatusTrue(),HttpStatus.OK);
+    }
+
+    @GetMapping("/priority/{id}")
+    public ResponseEntity<List<Task>> readAllTaskByStatusTrueAndByPriority(@PathVariable Integer id){
+        return new ResponseEntity<>(taskService.readAllTaskByStatusTrueAndByPriority(id),HttpStatus.OK);
+    }
+
+    @GetMapping("/sort")
+    public ResponseEntity<List<Task>> readAllSortTaskByStatusTrueAndByPriority(){
+        return new ResponseEntity<>(taskService.readAllSortTaskByStatusTrueAndByPriority(),HttpStatus.OK);
+    }
+
+    @GetMapping("/today")
+    public ResponseEntity<List<Task>> readTasksToday(){
+
+        return new ResponseEntity<>(taskService.readTasksByDateTodayAndStatusTrue(), HttpStatus.OK);
+    }
+    @GetMapping("/{date}")
+    public ResponseEntity<List<Task>> readTasksByDate(@PathVariable String date){
+        return new ResponseEntity<>(taskService.readTasksByDateAdnStatusTrue(date), HttpStatus.OK);
     }
 
     @PutMapping
     public ResponseEntity<Task> updateTask(@RequestBody Task task){
         return new ResponseEntity<>(taskService.updateTask(task),HttpStatus.OK);
     }
+
     @DeleteMapping("/{id}")
     public HttpStatus deleteTask(@PathVariable Long id){
         taskService.deleteTask(id);
--- backend/src/main/java/com/example/backend/service/TaskService.java
+++ backend/src/main/java/com/example/backend/service/TaskService.java
@@ -6,6 +6,8 @@ import com.example.backend.repository.TaskRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -20,12 +22,33 @@ public class TaskService {
                 .status(dto.getStatus())
                 .description(dto.getDescription())
                 .priority(dto.getPriority())
+                .user(dto.getUser())
+                .date(dto.getDate())
                 .build();
         return taskRepository.save(task);
     }
 
     public List<Task> readAllTask(){
         return taskRepository.findAll();
+    }
+
+    public List<Task> readAllTaskByStatusTrue(){return taskRepository.findByStatusTrue();}
+
+    public List<Task> readAllTaskByStatusTrueAndByPriority(Integer id){
+        return taskRepository.findByStatusTrueAndPriority(id);
+    }
+
+    public List<Task> readAllSortTaskByStatusTrueAndByPriority(){
+        List<Task> sortTasks = taskRepository.findByStatusTrue();
+        sortTasks.sort(Comparator.comparing(Task::getPriority));
+        return sortTasks;
+    }
+
+    public List<Task> readTasksByDateTodayAndStatusTrue(){
+        return taskRepository.findByDateAndStatusTrue(String.valueOf(LocalDate.now()));
+    }
+    public List<Task> readTasksByDateAdnStatusTrue(String date){
+        return taskRepository.findByDateAndStatusTrue(date);
     }
 
     public Task updateTask(Task task){
