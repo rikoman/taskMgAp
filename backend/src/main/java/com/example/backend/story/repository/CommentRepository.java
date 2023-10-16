52c04347d1757beccaa6cf57ed9ddb536d89ca45
roman <rikoman702@gmail.com>
2023-10-16 08:37:08 +0700

    Добавление аннотации @Repository

--- backend/src/main/java/com/example/backend/story/repository/CommentRepository.java
+++ backend/src/main/java/com/example/backend/story/repository/CommentRepository.java
@@ -2,9 +2,11 @@ package com.example.backend.story.repository;
 
 import com.example.backend.story.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
     List<Comment> findByProjectId(Long id);
