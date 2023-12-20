545462a6e38323cf51ee7306de36c26f04d8a908
roman <rikoman702@gmail.com>
2023-12-20 13:33:58 +0700

    Добавление обработчиков исключений

--- backend/src/main/java/com/example/backend/api/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/backend/api/exception/GlobalExceptionHandler.java
@@ -3,14 +3,20 @@ package com.example.backend.api.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
@@ -49,9 +55,59 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 
-    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex){
-       ErrorResponse errorResponse = new ErrorResponse("Страница не найдена");
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex){
+       ErrorResponse errorResponse = new ErrorResponse(ex.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    public ResponseEntity<Map<String,Object>> handleMissingServletRequestParameterException(HttpServletRequest request, MissingServletRequestParameterException ex){
+        Map<String,Object> body = new HashMap<>();
+        body.put("status",HttpStatus.BAD_REQUEST);
+        body.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path",request.getServletPath());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String,Object>> handleConstraintViolationException(HttpServletRequest request,ConstraintViolationException ex) {
+        Map<String,Object> body = new HashMap<>();
+
+        List<String> errors = new ArrayList<>();
+
+        for (ConstraintViolation<?> violation : ex.getConstraintViolations()) {
+            String errorMessage = violation.getPropertyPath() + " " + violation.getMessage();
+            errors.add(errorMessage);
+        }
+
+        body.put("status",HttpStatus.BAD_REQUEST);
+        body.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", errors);
+        body.put("path",request.getServletPath());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<Map<String,Object>> handleHttpMessageNotReadableException(HttpServletRequest request,HttpMessageNotReadableException ex) {
+        Map<String,Object> body = new HashMap<>();
+        body.put("status",HttpStatus.BAD_REQUEST);
+        body.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path",request.getServletPath());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String,Object>> handleIllegalArgumentException(HttpServletRequest request,IllegalArgumentException ex) {
+        Map<String,Object> body = new HashMap<>();
+        body.put("status",HttpStatus.BAD_REQUEST);
+        body.put("error",HttpStatus.BAD_REQUEST.getReasonPhrase());
+        body.put("message", ex.getMessage());
+        body.put("path",request.getServletPath());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
     }
 }
